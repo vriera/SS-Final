9fab68125feb108b1fb99283cfd36e0ec7b05c7f
Pedro <pdihax@gmail.com>
2024-02-11 15:20:00 -0300

    Fix for in generate grid and some cleaning

--- untitled/src/ar/edu/itba/models/Node.java
+++ untitled/src/ar/edu/itba/models/Node.java
@@ -7,22 +7,17 @@ public class Node{
     private int id;
     private double x;
     private double y;
+    private List<Road> outboundRoads;
+    private List<Road> inboundRoads;
 
 
     public List<Road> getOutboundRoads() {
         return outboundRoads;
     }
-
     public List<Road> getInboundRoads() {
         return inboundRoads;
     }
 
-    private List<Road> outboundRoads;
-    private List<Road> inboundRoads;
-    @Override
-    public String toString(){
-        return "Node[id=" +id + ",x=" + x + ",y=" + y + "]";
-    }
 
     public Node(int id,double x, double y){
         this.id = id;
@@ -41,8 +36,12 @@ public class Node{
         return x;
     }
     public double y(){
-
         return y;
+    }
+
+    @Override
+    public String toString(){
+        return "Node[id=" +id + ",x=" + x + ",y=" + y + "]";
     }
     @Override
     public boolean equals(Object o ){
